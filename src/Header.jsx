d8989384a3c3443962dcbb596c72906c96cd3243
Georgiy-777 <demirov709079@gmail.com>
2022-09-01 03:03:43 +0300

    prod

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import  * as React from 'react';
+// import  * as ReactDom from 'react-dom';
+export function Header() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+        </div>
+    );
+}
+
+export default Header;
